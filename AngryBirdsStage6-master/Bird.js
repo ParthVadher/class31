30833acd8689d2c10c40b62a43bf010a67a1bb61
ParthVadher <71433703+ParthVadher@users.noreply.github.com>
2020-12-19 12:33:27 +0530

    Add files via upload

---
+++ AngryBirdsStage6-master/Bird.js
@@ -0,0 +1,30 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+   
+    this.smokeI = loadImage("sprites/smoke.png");
+
+this.t=[]
+
+
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+    if(this.body.velocity.x>10&&this.body.position.x>200){
+
+    
+    var pos=[this.body.position.x,this.body.position.y]
+    this.t.push(pos);
+for(var i=0;i<this.t.length;i++){
+image(this.smokeI,this.t[i][0],this.t[i][1])
+
+}
+
+    }
+  }
+}
